b0806d8bc41d91ee892d09ce9d27b59986d4f66a
aramtantushyan <60556457+aramtantushyan@users.noreply.github.com>
2020-06-07 11:41:03 +0400

    Update Map(parseInteger).js

--- Homework 7/Tasks/Map(parseInteger).js
+++ Homework 7/Tasks/Map(parseInteger).js
@@ -1,6 +1,6 @@
 function parseInteger (array) {
     return array.map((string, i, array) => 
-        if(Number.isNaN(parseInt(string)) {
+        if(Number.isNaN(parseInt(string) )) {
             return null;
         } else {
             return parseInt(string);
