c86cd6ed50b6e5adcb02e150ba40259c6f78163f
aramtantushyan <60556457+aramtantushyan@users.noreply.github.com>
2020-07-17 03:13:03 +0400

    add new methods

--- Homework 16/Map/Dictionary.js
+++ Homework 16/Map/Dictionary.js
@@ -1,3 +1,43 @@
 class Dictionary {
+    constructor () {
+        this.myMap = new Map ();
+    }
     
+    getByKey (key) {
+        if (!myMap.has(key)) {
+            return undefined;
+        } else if (this.myMap.get(key).length = 0) {
+            return 'no value';
+        } else if (this.myMap.get(key).length = 1) {
+            return this.myMap.get(key)[0];
+        } else {
+            return this.myMap.get(key);
+        }
+    }
+
+    add (key, value) {
+        if(!myMap.has(key)) {
+            myMap.set(key, [value]);
+        }
+        myMap.set(key, [...myMap.get(key), value]);
+    }
+
+    remove (key) {
+        myMap.delete(key);
+    }
+
+    getCountByKey(key) {
+        if (!myMap.has(key)) {
+            return 0;
+        }
+            return myMap.get(key).length;
+    }
+
+    keys() {
+        return myMap.keys();
+    }
+
+    values() {
+        return myMap.values();
+    }
 }
