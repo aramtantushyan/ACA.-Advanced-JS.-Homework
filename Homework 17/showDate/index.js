43edc68987840bc57db23c9f836b1bea1e5f202e
aramtantushyan <60556457+aramtantushyan@users.noreply.github.com>
2020-07-26 14:56:43 +0400

    some fixes

--- Homework 17/showDate/index.js
+++ Homework 17/showDate/index.js
@@ -1,16 +1,10 @@
 function showDate () {
     let newDate = new Date ();
-    let dateBlock = mainBody.querySelector('.main-body time');
-    if (dateBlock) {
-        dateBlock.innerHTML = newDate;
-    } else {
-        time = document.createElement('time');
-        mainBody.append(time);
-        mainBody.querySelector('.main-body time').innerHTML = newDate;
-    }
+    dateBlock.textContent = newDate;
 }
 
 let mainBody = document.querySelector('.main-body');
 let dateButton = document.querySelector('.main-body button');
+let dateBlock = mainBody.querySelector('.main-body time');
 
 dateButton.addEventListener('click', showDate);
