66827e31f5f40f6c2cc762a58f07499c8757719b
aramtantushyan <60556457+aramtantushyan@users.noreply.github.com>
2020-07-27 03:16:04 +0400

    some fixes

--- Homework 17/listModification/index.js
+++ Homework 17/listModification/index.js
@@ -12,17 +12,21 @@ function markAsDone (event) {
     if(itemId === 0 || itemId > 0 && state[itemId - 1].isFinished === true) {
         state[itemId].isFinished = true;
         listItems[itemId].innerHTML = `<del>${listItems[itemId].textContent}</del>`
+    } else {
+        alert('You must accomplish the previous point(s)');
     }
-    alert('You must accomplish the previous point');
     isScheduleComplete(state);
 }
 
-isScheduleComplete (state) {
-    let leftTasks = state.filter((element) => {element.isFinished !== true});
-    if (leftTasks.length === 0) {
-        alert("You have completed today's tasks");
+function isScheduleComplete (state) {
+    for(let el of state) {
+        if(el.isFinished === false) {
+            return;
+        }
     }
+    alert("You have completed today's tasks");
 }
+
 
 let schedule = document.querySelector('.schedule')
 schedule.addEventListener('click', markAsDone)
