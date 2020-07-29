cb5fd0826f14f40ebc7ba91c71804ac047d26e1d
aramtantushyan <60556457+aramtantushyan@users.noreply.github.com>
2020-07-30 03:10:38 +0400

    finished code

--- Homework 17/modifyParagraphs/index.js
+++ Homework 17/modifyParagraphs/index.js
@@ -1,7 +1,7 @@
 function validateHexBody (input) {
     let lowercaseStr = input.toLowerCase();
     let result = true;
-    let digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     let letters = ['a', 'b', 'c', 'd', 'e', 'f']
     for (let char of lowercaseStr) {
         if (!(digits.includes(char) || letters.includes(char))) {
@@ -27,8 +27,20 @@ function validateInputs (color1, color2) {
     return (isHexColor(color1) && isHexColor(color2))
 }
 
+function modifyColors () {
+    if (validateInputs(textColor.value, backgroundColor.value)) {
+        Array.from(paragraphs).forEach((element) => {
+            element.style.color = textColor.value;
+            element.style.backgroundColor = backgroundColor.value;
+        })
+        return;
+    }
+    alert('Wrong inputs');
+}
+
 let textColor = document.querySelector('.inputs').firstElementChild; 
 let backgroundColor = document.querySelector('.inputs').lastElementChild; 
-let apply = document.querySelecto('.applyBtn').lastElementChild;
+let apply = document.querySelector('.applyBtn').lastElementChild;
+let paragraphs = document.querySelector('.main-paragraphs').children;
 
-apply.addEventListener
+apply.addEventListener('click', modifyColors);
