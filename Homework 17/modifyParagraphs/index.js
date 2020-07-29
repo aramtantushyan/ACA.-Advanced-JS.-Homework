8a86f952e08f6ab20f126084e4b0944b53ace466
aramtantushyan <60556457+aramtantushyan@users.noreply.github.com>
2020-07-30 02:47:02 +0400

    added files and code

---
+++ Homework 17/modifyParagraphs/index.js
@@ -0,0 +1,34 @@
+function validateHexBody (input) {
+    let lowercaseStr = input.toLowerCase();
+    let result = true;
+    let digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let letters = ['a', 'b', 'c', 'd', 'e', 'f']
+    for (let char of lowercaseStr) {
+        if (!(digits.includes(char) || letters.includes(char))) {
+            result = false;
+            return result;
+        }
+    }
+    return result;
+}
+
+function isHexColor (color) {
+    if (color[0] === '#' && 
+        color.length <=7 && 
+        color.trim() !== '' && 
+        typeof color === 'string' &&
+        validateHexBody(color.slice(1))) {
+        return true;
+    }
+    return false;
+}
+
+function validateInputs (color1, color2) {
+    return (isHexColor(color1) && isHexColor(color2))
+}
+
+let textColor = document.querySelector('.inputs').firstElementChild; 
+let backgroundColor = document.querySelector('.inputs').lastElementChild; 
+let apply = document.querySelecto('.applyBtn').lastElementChild;
+
+apply.addEventListener
