0c61ad566d9456aac3b40c893c91faee9a7471df
aramtantushyan <60556457+aramtantushyan@users.noreply.github.com>
2020-08-10 22:09:32 +0400

    current commit

--- Homework 18/getMoviesList(task1&4)/index.js
+++ Homework 18/getMoviesList(task1&4)/index.js
@@ -1,2 +1,4 @@
-import {doGet} from ".requestHelper/request.helper.js";
- 
+console.log('hello')
+import { doGet } from "../requestHelper/request.helper.js";
+
+doGet('https://ghibliapi.herokuapp.com/films').catch((e)=> console.log(e));
