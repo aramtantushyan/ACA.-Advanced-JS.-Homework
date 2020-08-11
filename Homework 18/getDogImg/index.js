2cad5bd83d82b30cb617b02f94c48bfec13b6f2b
aramtantushyan <60556457+aramtantushyan@users.noreply.github.com>
2020-08-11 16:52:43 +0400

    added images and finishe task

--- Homework 18/getDogImg/index.js
+++ Homework 18/getDogImg/index.js
@@ -1,11 +1,17 @@
 import { doGet }  from "../requestHelper/request.helper.js";
 
 const img = document.querySelector('.main-body__dog_img');
+const selector = document.getElementById('dogs');
 
-function getDogImg (value) {
-    if(value !== 'default') {
-        doGet(`https://dog.ceo/api/breed/${value}/images/random`)
+function getDogImg () {
+    const selectedBreed = selector.value;
+    if(selectedBreed !== 'default') {
+        doGet(`https://dog.ceo/api/breed/${selectedBreed}/images/random`)
         .then(res => {img.src = res.message})
         .catch(err => console.log(err));
+    } else {
+        img.src = './img/img_placeholder.png';
     }
 }
+
+selector.addEventListener('change', getDogImg);
