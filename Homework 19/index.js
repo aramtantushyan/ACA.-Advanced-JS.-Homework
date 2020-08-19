6cc029ddc3f157eae0b57deb4b56c4bb68a88d06
aramtantushyan <60556457+aramtantushyan@users.noreply.github.com>
2020-08-19 16:16:02 +0400

    added event listeners for inputs and made fixes

--- Homework 19/index.js
+++ Homework 19/index.js
@@ -1,6 +1,8 @@
 const eyeIcon = document.querySelector('.ion-icon__eye');
-const password = document.getElementById('password');
-const username = document.getElementById('username');
+const username = document.getElementById('1');
+const password = document.getElementById('2');
+const errorText1 = document.querySelector('.error1');
+const loginBlock = document.querySelector('.main-body__login');
 
 function showHidePassword () {
     if (password.type === "password") {
@@ -13,19 +15,37 @@ function showHidePassword () {
 }
 
 function validateInput (target) {
-    let inputValue = target.value;
-    let iconId = (target.id === 'username') ? 1 : 2;
-    let check = document.querySelector(`.ion-icon${iconId}_check`); 
-    let close = document.querySelector(`.ion-icon${iconId}_close`); 
-    if (inputValue.length < 5) {
+    const inputValue = target.value;
+    const targetId = target.id;
+    const check = document.querySelector(`.ion-icon${targetId}_check`); 
+    const close = document.querySelector(`.ion-icon${targetId}_close`);
+    const errorText = document.querySelector(`.error${targetId}`);
+    if (inputValue.trim().length < 5) {
         close.classList.add('visible');
         check.classList.remove('visible');
+        errorText.classList.add('visible');
+        return false;
     } else {
         check.classList.add('visible');
         close.classList.remove('visible');
+        errorText.classList.remove('visible');
+        return true;
     }
 }
 
+function validateUsernamePassword () {
+    return (validateInput(username) && validateInput(password));
+}
+
+username.addEventListener('keyup', (e) => {validateInput(e.target)})
+password.addEventListener('keyup', (e) => {validateInput(e.target)})
 username.addEventListener('blur', (e) => {validateInput(e.target)});
 password.addEventListener('blur', (e) => {validateInput(e.target)});
+loginBlock.addEventListener('keydown', (e) => {
+    if(e.key === 'return') {
+        if(validateUsernamePassword) {
+            
+        }
+    }
+})
 eyeIcon.addEventListener('click', showHidePassword);
