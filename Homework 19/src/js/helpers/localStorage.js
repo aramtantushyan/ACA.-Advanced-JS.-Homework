f84749d21598b9ab19af7947dbeacb4195787830
aramtantushyan <60556457+aramtantushyan@users.noreply.github.com>
2020-08-24 00:46:25 +0400

    added local storage for helper

---
+++ Homework 19/src/js/helpers/localStorage.js
@@ -0,0 +1,7 @@
+function setToLocalStorage (key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
+function getFromLocalStorage (key) {
+    return JSON.parse(localStorage.getItem(key));
+}
