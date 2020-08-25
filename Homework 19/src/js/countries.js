7cb77d5649ba3b184ef15690554ee970e0c276dc
aramtantushyan <60556457+aramtantushyan@users.noreply.github.com>
2020-08-26 03:16:25 +0400

    lot of bug fixes

--- Homework 19/src/js/countries.js
+++ Homework 19/src/js/countries.js
@@ -1,33 +1,47 @@
 import {renderCountryCards} from './helpers/render.helper.js';
 import {debounce} from './helpers/debounce.helper.js';
 import {getFromLocalStorage} from './helpers/localStorage.js';
-import {getAllCountries} from './services/getCountriesData.js';
-import {getCountryByName} from './services/getCountriesData.js';
+import {setToLocalStorage} from './helpers/localStorage.js'
+import {fetchAllCountries} from './services/fetchCountriesData.js';
+import {fetchCountryByName} from './services/fetchCountriesData.js';
 
 const mainBody = document.querySelector('.main-body');
-const viewCountry = document.querySelector('.view');
-const favIcon = document.querySelector('.fav');
 const search = document.getElementById('search');
 const searchBtn = document.getElementById('searchBtn');
 
+let favIcon;
+let viewCountry;
+
+const state = {};
+
 function initPage () {
-    let allCountriesData = getFromLocalStorage('allCountries');
-    if (allCountriesData.length !== 0) {
-        mainBody.innerHTML = '';
-        renderCountryCards(mainBody, 'beforeend', allCountriesData);
-    } else {
-        getAllCountries();
-        allCountriesData = getFromLocalStorage('allCountries');
-        renderCountryCards(mainBody, 'beforeend', allCountriesData);
+    try {
+        let allCountriesData = getFromLocalStorage('allCountries');
+        if (allCountriesData && allCountriesData.length !== 0) {
+            mainBody.innerHTML = '';
+            renderCountryCards(mainBody, 'beforeend', allCountriesData);
+        } else {
+            fetchAllCountries();
+            //wait some time for ferchAllCountries function to write data to local storage
+            setTimeout(() => {allCountriesData = getFromLocalStorage('allCountries');
+            renderCountryCards(mainBody, 'beforeend', allCountriesData)}, 250);
+            location.reload();   
+        }
+        favIcon = document.querySelectorAll('.fav');
+        viewCountry = document.querySelectorAll('.view');
+        favIcon.forEach((e) => {e.addEventListener('click', (event) => addToFavourites(event.target.id)), console.log(1) });
+        viewCountry.forEach((e) => {e.addEventListener('click', (event)=> console.log(event.target.id)) });
+    } catch (error) {
+        console.log(error);
     }
 }
 
 function showSearchResults () {
-    getCountryByName(search.value);
+    fetchCountryByName(search.value);
     const searchResults = getFromLocalStorage('searchResults');
     console.log(searchResults);
     mainBody.innerHTML = '';
-    if (searchResults.length === 0) {
+    if (searchResults && searchResults.length === 0) {
         mainBody.insertAdjacentHTML('beforeend', '<h1>0 results match your search.</h1>');
     } else if (search.value.trim() === '') {
         initPage();
@@ -37,8 +51,32 @@ function showSearchResults () {
     }
 }
 
+function getCountryData (country) {
+    const allCountries = getFromLocalStorage('allCountries');
+    for (let elem of allCountries) {
+        if(elem.name === country) {
+            return elem;
+        }
+    }
+}
+
+function addToFavourites (country) {
+    const favCountryData = getCountryData(country);
+    const favourites = getFromLocalStorage('favourites');
+    if (favourites) {
+        for (let elem of favourites) {
+            if(elem.name === country) {
+                return;
+            }
+        }
+        const newFavourites = [...favourites, favCountryData];
+        setToLocalStorage('favourites', newFavourites);
+    } else {
+        setToLocalStorage('favourites', [favCountryData]);
+    }
+}
+
 initPage();
 
 search.addEventListener('keyup', debounce(showSearchResults, 200));
 searchBtn.addEventListener('click', debounce(showSearchResults, 200));
-
