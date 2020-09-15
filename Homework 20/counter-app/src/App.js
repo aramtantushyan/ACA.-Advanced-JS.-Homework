95dfbd9ce4cc567e27dcf33f83f423fe01d30468
aramtantushyan <60556457+aramtantushyan@users.noreply.github.com>
2020-09-15 23:46:07 +0400

    initial commit

---
+++ Homework 20/counter-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <button>Increase</button>
+      <button>Decrease</button>
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
